dc251cc14e0282e6de568ef5a8337b6d6c0cd4f4
sydneyleeATX <sydney.r.lee04@gmail.com>
2025-06-17 15:10:25 -0500

    Add vite-plugin-pwa for full PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    open: true,
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        // You can omit this if you want to use your existing public/manifest.json,
+        // or you can copy its contents here for easier management.
+      }
+    })
+  ],
 });
